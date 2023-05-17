72f4aac206e33d0087c36025246cadd606daaf49
huakucha <zhuyuliangm@outlook.com>
2023-05-17 22:04:45 +0800

    init

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,74 @@
-import { window } from 'vscode'
+import fs from 'node:fs'
+import path from 'node:path'
+import { cpus } from 'node:os'
+import { ImagePool } from '@squoosh/lib'
+import { commands, window } from 'vscode'
+import { type ExtensionContext } from 'vscode'
+import recursive from 'recursive-readdir'
 
-export function activate() {
-  window.showInformationMessage('Hello')
+const supportExtensions = ['.webp', '.png', '.jpg', 'jpeg']
+
+async function compress(imageFiles: Array<string>) {
+  const imagePool = new ImagePool(cpus().length)
+  for (let index = 0; index < imageFiles.length; index++) {
+    const imageFile = imageFiles[index]
+    const extension = path.extname(imageFile)
+    const fileName = imageFile.replace(extension, '')
+    const file = await fs.readFileSync(imageFile)
+    const image = await imagePool.ingestImage(file)
+    const encodeOptions = {
+      webp: {
+        quality: 75,
+        effort: 6,
+      },
+    }
+    await image.encode(encodeOptions)
+    const rawData = (await image.encodedWith.webp)?.binary
+    if (rawData)
+      fs.writeFileSync(`${fileName}.webp`, rawData)
+  }
+  imagePool.close()
+  window.showInformationMessage('压缩完成...')
 }
 
-export function deactivate() {
+function ignoreFunc(file: string, stats: fs.Stats) {
+  const ext = path.extname(file)
+  if (file.endsWith('DS_Store'))
+    return true
+  if (stats.isFile() && !supportExtensions.includes(ext))
+    return true
+  return false
+}
 
+function activate(context: ExtensionContext) {
+  const disposable = commands.registerCommand('convert', async (obj) => {
+    const filePath = obj.path
+    const ext = path.extname(filePath)
+
+    // folder
+    if (ext === '') {
+      recursive(filePath, [ignoreFunc], (err, files) => {
+        if (err)
+          window.showErrorMessage('发生错误了', err.message)
+        compress(files)
+      })
+    }
+
+    // file
+    else if (supportExtensions.includes(ext)) {
+      compress([filePath])
+    }
+
+    else {
+      window.showErrorMessage('not support file')
+    }
+  })
+  context.subscriptions.push(disposable)
+}
+
+function deactivate() {}
+
+module.exports = {
+  activate,
+  deactivate,
 }
